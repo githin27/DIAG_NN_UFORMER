6fbca81131a767474c5498f9db97b0a59a73a158
Githin K Suresh <66995966+githin27@users.noreply.github.com>
2023-08-27 12:52:41 +0200

    Update custom_dataset_deblur.sh

--- development/scripts/custom_dataset_deblur.sh
+++ development/scripts/custom_dataset_deblur.sh
@@ -2,32 +2,33 @@
 
 #creating train dataset
 python ./generate_patch/custom_dataset_deblur.py \
-    --patch_src_gt ./rough_GoPro/train/groundtruth/ \
-    --patch_src_in ./rough_GoPro/train/input/ \
-    --patch_tar ./datasets/rough/deblur/train \
-    --ps 64 \
-    --num_patches 5 \
-    --num_cores 2
+    --patch_src_gt ./GoPro/train/groundtruth/ \
+    --patch_src_in ./GoPro/train/input/ \
+    --patch_tar ./datasets/deblur/GoPro/customized_dataset/train \
+    --ps 128 \
+    --num_patches 50 \
+    --num_cores 8
     
 
 """
-creating validation dataset
+#creating validation dataset
 python ./generate_patch/custom_dataset_deblur.py \
-    --patch_src_gt ./rough_GoPro/train/groundtruth/ \
-    --patch_src_in ./rough_GoPro/train/input/ \
-    --patch_tar ./datasets/rough/deblur/val \
+    --patch_src_gt ./GoPro/train/groundtruth/ \
+    --patch_src_in ./GoPro/train/input/ \
+    --patch_tar ./datasets/deblur/GoPro/customized_dataset/val \
     --ps 128 \
     --num_patches 20 \
-    --num_cores 2
+    --num_cores 8
 
 
-creating test dataset
+#creating test dataset
 python ./generate_patch/custom_dataset_deblur.py \
-    --patch_src_gt ./rough_GoPro/train/groundtruth/ \
-    --patch_src_in ./rough_GoPro/train/input/ \
-    --patch_tar ./datasets/rough/deblur \
+    --patch_src_gt ./GoPro/test/groundtruth/ \
+    --patch_src_in ./GoPro/test/input/ \
+    --patch_tar ./datasets/deblur/GoPro/customized_dataset/test \
     --ps 128 \
     --num_patches 20 \
-    --num_cores 2
+    --num_cores 8
+"""
 
 read -p "Press Enter to exit..."
